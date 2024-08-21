6adbe6015ca3d22cba2191298d983920852be916
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2024-08-21 11:25:24 +0530

    Misc

--- game.js
+++ game.js
@@ -26,7 +26,7 @@ let ravenInterval = 950;
 let lastTime = 0;
 let score = 0;
 let gameOver = false;
-let lives = 5000;
+let lives = 5;
 
 // ARRAY TO STORE RAVEN OBJECTS
 let ravens = [];
@@ -38,7 +38,7 @@ class Raven {
     this.spriteHeight = 194;
     this.sizeModifier = Math.random() * 0.6 + 0.4;
     this.width = this.spriteWidth * this.sizeModifier;
-    this.height = this.spriteHeight * this.sizeModifier; //Raven class properties and methods
+    this.height = this.spriteHeight * this.sizeModifier; 
     this.x = canvas.width;
     this.y = Math.random() * (canvas.height - this.height);
     this.directionX = Math.random() * 5 + 3;
@@ -70,7 +70,7 @@ class Raven {
     if (this.y < 0 || this.y > canvas.height) {
       this.directionY = this.directionY * -1;
     }
-    this.x -= this.directionX; //Update logic for Raven objects
+    this.x -= this.directionX; 
     this.y -= this.directionY;
     if (this.x < 0 - this.width) this.markedForDeletion = true;
     this.timeSinceFlap += deltatime;
@@ -95,7 +95,7 @@ class Raven {
 
   draw() {
     collisionCtx.fillStyle = this.color;
-    collisionCtx.fillRect(this.x, this.y, this.width, this.height); //Draw logic for Raven objects
+    collisionCtx.fillRect(this.x, this.y, this.width, this.height); 
     ctx.drawImage(
       this.image,
       this.frame * this.spriteWidth,
@@ -120,7 +120,7 @@ class Explosion {
     this.spriteHeight = 179;
     this.x = x;
     this.y = y;
-    this.size = size; //Explosion class properties and methods
+    this.size = size; 
     this.frame = 0;
     this.sound = new Audio();
     this.sound.src = "assets/gunshot.mp3";
